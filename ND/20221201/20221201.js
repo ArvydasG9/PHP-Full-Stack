42840ae97c814c6057b1709f0fd15b0a1174a91b
ArvydasG9 <117637633+ArvydasG9@users.noreply.github.com>
2022-12-01 19:38:28 +0200

    ND20221201 korekcija

--- ND/20221201/20221201.js
+++ ND/20221201/20221201.js
@@ -16,6 +16,7 @@ if (Math.min(n1, n2) === 0) {
 }
 if (n1 > n2) n1 /= n2;
 else n1 = n2 / n1;
+// n1 = Math.round(n1);
 console.log(n1);
 n1 = n1.toFixed(2);
 console.log(n1);
@@ -27,9 +28,10 @@ let k3 = rand(0, 25);
 console.log(k1);
 console.log(k2);
 console.log(k3);
-if (Math.max(k2, k3) > k1 && Math.min(k2, k3) < k1) console.log(k1);
-else if (Math.max(k1, k3) > k2 && Math.min(k1, k3) < k2) console.log(k2);
-else if (Math.max(k1, k2) > k3 && Math.min(k1, k2) < k3) console.log(k3);
+if (k1 == k2 || k1 == k3 || k2 == k3) console.log("Vidytinės reikšmės nėra");
+else if (Math.max(k2, k3) > k1 && Math.min(k2, k3) < k1) console.log("Vidytinė reikšmė: " + k1);
+else if (Math.max(k1, k3) > k2 && Math.min(k1, k3) < k2) console.log("Vidytinė reikšmė: " + k2);
+else if (Math.max(k1, k2) > k3 && Math.min(k1, k2) < k3) console.log("Vidytinė reikšmė: " + k3);
 // ----   3  --------------
 console.log("Užduotis - 3");
 const vardasAkt = "Jean-Paul";
@@ -40,5 +42,7 @@ let res = vardasAkt.substring(0, 3) + pavardeAkt.substring(0, 3)
 console.log(res);
 // ----   4  --------------
 console.log("Užduotis - 4");
-let s=String.fromCharCode(rand(65, 90))+String.fromCharCode(rand(65, 90))+String.fromCharCode(rand(65, 90))
-console.log(s.toLowerCase());
+// let s = String.fromCharCode(rand(65, 90)) + String.fromCharCode(rand(65, 90)) + String.fromCharCode(rand(65, 90))
+// console.log(s.toLowerCase());
+let s = String.fromCharCode(rand(97, 122)) + String.fromCharCode(rand(97, 122)) + String.fromCharCode(rand(97, 122))
+console.log(s);
