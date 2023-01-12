52eda95c084a8895aab9c5d19e9e96e196310a0a
ArvydasG9 <117637633+ArvydasG9@users.noreply.github.com>
2023-01-12 18:40:46 +0200

    pakeitimai

--- ND/E2React/20230112react/src/App.js
+++ ND/E2React/20230112react/src/App.js
@@ -1,14 +1,108 @@
-
 import './App.css';
+import { useId, useState } from 'react';
+import { IoCloseCircleSharp, IoBuild, IoCheckmark } from 'react-icons/io5'
 
-function App() {
+const Header = ({ nav }) => {
+  const id = useId();
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <header>
+      <h1>UAB "Šaunu"</h1>
+      <nav>
+        <ul>
+          {nav.map((value, index) => <li key={id + index}>{value}</li>)}
+        </ul>
+      </nav>
+    </header>
+  );
+}
 
-<h1>Ups</h1>
-      </header>
-    </div>
+const Container = (propsai) => {
+  return <div className="container">{propsai.children}</div>;
+}
+
+// const FormEntry = () => {
+//   const [reiksme, setReiksme] = useState('Įveskite tekstą į laukelį');
+
+//   const keistiReiksme = (e) => {
+//     console.log(e.target.value.length);
+//     setReiksme(e.target.value);
+//   }
+
+//   return (
+//     <>
+//       <input type="text" onChange={keistiReiksme} />
+//       {/* <input type="text" onChange={(e) => setReiksme(e.target.value)} /> */}
+//       <div className="result">{reiksme}</div>
+//     </>
+//   );
+// }
+
+const FormEntry = () => {
+  const [tasks, setTasks] = useState([]);
+  const [currentTask, setCurrentTask] = useState();
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    console.log(tasks)
+    console.log(currentTask)
+    console.log(e)
+
+    setTasks([...tasks, { name: currentTask, done: false }]);
+  }
+
+  const handleDone = (e, index) => {
+    tasks[index].done = !tasks[index].done;
+    setTasks([...tasks]);
+  }
+
+  const handleEdit = (e, index) => {
+    console.log(e)
+    console.log(index)
+    tasks[index].name = setCurrentTask(e.target.value);
+    setTasks([...tasks]);
+  }
+
+  const handleDelete = (e, index) => {
+    tasks.splice(index, 1);
+    setTasks([...tasks]);
+  }
+
+  return (
+    <>
+      <h1>Task Manager</h1>
+      <form className="input-group" >
+        <input type="text" className="form-control" onChange={(e) => setCurrentTask(e.target.value)} />
+        <button className="btn btn-primary" onClick={handleForm}>Išsaugoti</button>
+      </form>
+      {tasks.map((value, index) =>
+        // <li key={value.name + index} onClick={ (e) => handleDone(e, index) } className={value.done ? 'done' : ''}>{value.name}</li>        
+        <div key={value.name + index} className={value.done ? 'done item' : 'item'}>
+          {value.name}
+          <div>
+            <IoCheckmark onClick={(e) => handleDone(e, index)} />
+            <IoBuild onClick={(e) => handleEdit(e, index)} />
+            <IoCloseCircleSharp onClick={(e) => handleDelete(e, index)} />
+          </div>
+
+
+        </div>
+
+      )}
+    </>
+  );
+}
+
+const App = () => {
+  const navigacija = ['Titulinis', 'Apie Mus', 'Kontaktai'];
+
+  return (
+    <Container>
+      <Header nav={navigacija} />
+      <main>
+        <FormEntry />
+      </main>
+    </Container>
   );
 }
 
